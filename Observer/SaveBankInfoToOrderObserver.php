1b2b4e9a955fa1994b082b3e6f7759c736e00369
Ubuntu <ubuntu@ip-172-26-0-84.us-west-2.compute.internal>
2019-06-14 00:20:08 +0000

    alteracoes para exemplo

---
+++ Observer/SaveBankInfoToOrderObserver.php
@@ -0,0 +1,44 @@
+<?php
+namespace Ibnab\Additional\Observer;
+use Magento\Framework\Event\Observer as EventObserver;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\OfflinePayments\Model\Banktransfer;
+class SaveBankInfoToOrderObserver implements ObserverInterface {
+
+    protected $_inputParamsResolver;
+    protected $_quoteRepository;
+    protected $logger;
+    protected $_state;
+
+    public function __construct(\Magento\Webapi\Controller\Rest\InputParamsResolver $inputParamsResolver, \Magento\Quote\Model\QuoteRepository $quoteRepository, \Psr\Log\LoggerInterface $logger,\Magento\Framework\App\State $state) {
+        $this->_inputParamsResolver = $inputParamsResolver;
+        $this->_quoteRepository = $quoteRepository;
+        $this->logger = $logger;
+        $this->_state = $state;
+    }
+
+    public function execute(EventObserver $observer) {
+
+        $inputParams = $this->_inputParamsResolver->resolve();
+        if($this->_state->getAreaCode() != \Magento\Framework\App\Area::AREA_ADMINHTML){
+        foreach ($inputParams as $inputParam) {
+	    
+            if ($inputParam instanceof \Magento\Quote\Model\Quote\Payment) {
+		//tratar aqui o post
+                $paymentData = $inputParam->getData('additional_data');
+		//print_r($paymentData);
+		
+                $paymentOrder = $observer->getEvent()->getPayment();
+                $order = $paymentOrder->getOrder();
+                $quote = $this->_quoteRepository->get($order->getQuoteId());
+                $paymentQuote = $quote->getPayment();
+                $method = $paymentQuote->getMethodInstance()->getCode();
+		
+                if ($method == Banktransfer::PAYMENT_METHOD_BANKTRANSFER_CODE) {
+		    $observer->getEvent()->getPayment()->setAdditionalInformation("adicional1", $paymentData['adicional1']);
+                }
+            }
+        }
+       }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+##Manual installation :
+
+forked by Joao Moreno from github (https://github.com/ibnab/payment-additional)
+
+past in your folder app/code
+
+enable extension in  app/etc/config.php by adding 'Ibnab_Additional' => 1,
+
+and execute the command php bin/magento setup:upgrade
